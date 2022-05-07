407ea50190a45ffabd9462de23d1ed289726a807
Nahuel61920 <estebancarrizo619@gmail.com>
2022-05-06 22:29:22 -0300

    Create spinners in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'
+
+/* Loading spinner */
+import GridLoader from "react-spinners/GridLoader";
+import RingLoader from "react-spinners/RingLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 4000)
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        loading ?
+
+          <div className='loading-pag'>
+            <GridLoader
+              color={"#00e5fe"}
+              loading={loading}
+              size={100}
+            />
+          </div>
+
+          :
+          <header className="App-header">
+            <div className='App-logo'>
+              <RingLoader
+                color={"#00e5fe"}
+                size={200}
+              />
+            </div>
+            <h1>Spinners</h1>
+          </header>
+      }
     </div>
   );
 }
